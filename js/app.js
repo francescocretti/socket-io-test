b708d3688039e05f483d6474fceee773c1fc4a80
francescocretti <francesco.cretti@gmail.com>
2019-05-24 18:11:26 +0200

    use square wave and interpolate frequencies

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
 
 var oscillator;
+var gain;
 
 var username;
 
@@ -37,48 +38,55 @@ $(function() {
 
 function initOscillator(ctx) {
   var osc = ctx.createOscillator();
-  osc.type = 'sine';
+  gain = ctx.createGain();
+  gain.gain.setValueAtTime(0, ctx.currentTime);
+  osc.type = 'square';
   osc.frequency.setValueAtTime(440, ctx.currentTime);
-  osc.connect(ctx.destination);
+  // osc.gain.setValueAtTime(0, ctx.currentTime);
+  osc.connect(gain);
+  gain.connect(ctx.destination);
+  osc.start();
   return osc;
 }
 
 function playSoundSignal(freq) {
-  // console.log(pos);
-  // var freq = Math.random() * (8000 - 500) + 500;
-  oscillator.frequency.setValueAtTime(freq, audioContext.currentTime);
-  oscillator.start();
-  // osc.stop(audioContext.currentTime + 2);
+  console.log('playing sound at ' + freq);
+  oscillator.frequency.setTargetAtTime(freq, audioContext.currentTime, 1);
+  gain.gain.setValueAtTime(1, audioContext.currentTime);
 }
 
 function initApp(socket) {
   // socket listeners
   socket.on('chat message', function(msg) {
-    // oscillator.frequency.setValueAtTime(parseInt(msg), audioContext.currentTime);
     $('#messages').append($('<li>').text(msg));
   });
+
   socket.on('play sound', function(baseFreq) {
     var intervals = [0, 2, 4, 5, 7, 9, 11];
     var i = intervals[Math.floor(Math.random() * intervals.length)];
     var freq = baseFreq * Math.pow(Math.pow(2, i), 1/12);
     playSoundSignal(freq);
   });
+
   // postiion
   function success(pos) {
-    var crd = pos.coords;
-    socket.emit('new pos', crd.latitude);
-    console.log(crd);
-    // navigator.geolocation.clearWatch(id);
-    // playPosSoundSignal(oscillator);
+    console.log(pos);
+    var crd = {};
+    crd.lat = pos.coords.latitude;
+    crd.lng = pos.coords.longitude;
+    crd.acc = pos.coords.accuracy;
+    socket.emit('new pos', crd);
   }
   function error(err) {
     console.warn('ERROR(' + err.code + '): ' + err.message);
   }
+
   options = {
     enableHighAccuracy: false,
     timeout: 1000,
     maximumAge: 0
   };
+
   id = navigator.geolocation.watchPosition(success, error, options);
   // audio
   oscillator = initOscillator(audioContext);
